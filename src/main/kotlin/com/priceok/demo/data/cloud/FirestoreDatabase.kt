a783492b8d3eb746ce56bd5e4d68eeacbe4d7c7e
Dmitry Sokolov <dmitry.sokolov@requestum.com>
2019-02-23 16:40:17 +0200

    Added firestore component.

---
+++ src/main/kotlin/com/priceok/demo/data/cloud/FirestoreDatabase.kt
@@ -0,0 +1,39 @@
+package com.priceok.demo.data.cloud
+
+import com.google.auth.oauth2.GoogleCredentials
+import com.google.cloud.firestore.Firestore
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Component
+import com.google.firebase.FirebaseApp
+import com.google.firebase.FirebaseOptions
+import com.google.firebase.cloud.FirestoreClient
+
+@Component
+class FirestoreDatabase @Autowired constructor(props: FirestoreProperties) {
+
+    private lateinit var db: Firestore
+
+    init {
+        val options = FirebaseOptions.Builder()
+                .setCredentials(GoogleCredentials.fromStream(props.credentialsStream))
+                .setProjectId(props.projectId)
+                .build().apply {
+                    FirebaseApp.initializeApp(this)
+                }
+        db = FirestoreClient.getFirestore()
+    }
+
+    final fun collection(name: String) = db.collection(name)
+
+    final fun insertData(collectionName: String, map: MutableMap<String, Any>)
+            = db.collection(collectionName).document().set(map)
+
+    final fun updateData(collectionName: String, documentName: String, map: MutableMap<String, Any>)
+            = db.collection(collectionName).document(documentName).set(map)
+
+    final fun getData(collectionName: String, documentName: String)
+            = db.collection(collectionName).document(documentName).get().get()
+
+    final fun deleteData(collectionName: String, documentName: String)
+            = db.collection(collectionName).document(documentName).delete().get()
+}
